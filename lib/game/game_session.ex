3bb5fb1d63944c9f9105c5ac77caf38adc39b7b3
Emil Kirilov <emil.kirilovk@gmail.com>
2017-07-03 23:41:30 +0300

    Identation to spaces.

--- lib/game/game_session.ex
+++ lib/game/game_session.ex
@@ -1,77 +1,77 @@
 defmodule ConnectFour.Game.GameSession do
-	use GenServer
-	use Tensor
+  use GenServer
+  use Tensor
 
-	def start_link do
-		GenServer.start_link(__MODULE__, [])
-	end
+  def start_link do
+    GenServer.start_link(__MODULE__, [])
+  end
 
-	def init(_) do
-		board = build_board()
-		{:ok, board}
-	end
+  def init(_) do
+    board = build_board()
+    {:ok, board}
+  end
 
-	def build_board() do
-		Matrix.new(10,10)
-	end
+  def build_board() do
+    Matrix.new(10,10)
+  end
 
-	def drop_disc(receiver_pid, picked_column) when is_integer(picked_column) do
-		# 0 < n < Matrix.width
-		GenServer.call(receiver_pid, {:drop_disc, picked_column})
-	end
+  def drop_disc(receiver_pid, picked_column) when is_integer(picked_column) do
+    # 0 < n < Matrix.width
+    GenServer.call(receiver_pid, {:drop_disc, picked_column})
+  end
 
 
-	def drop_disc(_receiver_pid, picked_column) do
-		error = "unexpected column number, received: (#{inspect(picked_column)})"
+  def drop_disc(_receiver_pid, picked_column) do
+    error = "unexpected column number, received: (#{inspect(picked_column)})"
     raise ArgumentError, error
-	end
+  end
 
-	# Callbacks
+  # Callbacks
 
-	def handle_call({:drop_disc, picked_column}, _from, board) do
-		# ako iska da oveflowne daskata?
+  def handle_call({:drop_disc, picked_column}, _from, board) do
+    # ako iska da oveflowne daskata?
 
-		# new_board = Matrix.columns(board) |>
-		# 						Enum.at(picked_column) |>
-		# 						Vector.reverse |>
-		# 						place_disc_in_first_free_spot |>
-		# 						Vector.reverse |>
-		# 						replace_column(board, picked_column) |>
-		# 						Matrix.new(10, 10) # height could change?
+    # new_board = Matrix.columns(board) |>
+    #             Enum.at(picked_column) |>
+    #             Vector.reverse |>
+    #             place_disc_in_first_free_spot |>
+    #             Vector.reverse |>
+    #             replace_column(board, picked_column) |>
+    #             Matrix.new(10, 10) # height could change?
 
-		new_board = Matrix.columns(board) |> 		# list of vectors
-								Enum.at(picked_column) |>		# vector
-								update_column	|>						# vector
-								update_board(board, picked_column)
+    new_board = Matrix.columns(board) |>    # list of vectors
+                Enum.at(picked_column) |>   # vector
+                update_column |>            # vector
+                update_board(board, picked_column)
 
-		{:reply, new_board, new_board}
-	end
+    {:reply, new_board, new_board}
+  end
 
-	# The column's content is listed from the top of the board towards the bottom
-	# therefore I am inversing the row
-	def first_empty_cell(column) do
-		Vector.length(column) - 1 -
-		(Vector.reverse(column) |>
-		Enum.find_index(fn (el) -> el == 0 end))
-	end
+  # The column's content is listed from the top of the board towards the bottom
+  # therefore I am inversing the row
+  def first_empty_cell(column) do
+    Vector.length(column) - 1 -
+    (Vector.reverse(column) |>
+    Enum.find_index(fn (el) -> el == 0 end))
+  end
 
-	def update_column(column) do
-		List.replace_at(
-			Vector.to_list(column),
-			first_empty_cell(column),
-			1
-		) |>
-		Vector.new
-	end
+  def update_column(column) do
+    List.replace_at(
+      Vector.to_list(column),
+      first_empty_cell(column),
+      1
+    ) |>
+    Vector.new
+  end
 
-	def update_board(new_column, board, index) do
-		List.replace_at(
-			Matrix.columns(board),
-			index,
-			new_column
-		) |>
-		Enum.map(fn (vector) -> Vector.to_list(vector) end) |>
-		Matrix.new(Matrix.height(board),Matrix.width(board)) |> # what if they change dinamically
-		Matrix.transpose
-	end
+  def update_board(new_column, board, index) do
+    List.replace_at(
+      Matrix.columns(board),
+      index,
+      new_column
+    ) |>
+    Enum.map(fn (vector) -> Vector.to_list(vector) end) |>
+    Matrix.new(Matrix.height(board),Matrix.width(board)) |> # what if they change dinamically
+    Matrix.transpose
+  end
 end
--- test/game/game_session_test.exs
+++ test/game/game_session_test.exs
@@ -1,80 +1,80 @@
 defmodule ConnectFour.Game.GameSessionTest do
-	use ExUnit.Case
-	use Tensor
-	alias ConnectFour.Game.GameSession
+  use ExUnit.Case
+  use Tensor
+  alias ConnectFour.Game.GameSession
 
-	doctest ConnectFour
+  doctest ConnectFour
 
-	describe "build_board/0" do
-		test "builds a 10x10 board" do
-			matrix = GameSession.build_board()
+  describe "build_board/0" do
+    test "builds a 10x10 board" do
+      matrix = GameSession.build_board()
 
-			row = Matrix.row(matrix, 0)
-			col = Matrix.column(matrix, 0)
+      row = Matrix.row(matrix, 0)
+      col = Matrix.column(matrix, 0)
 
-			assert Vector.length(row) == 10
-			assert Vector.length(col) == 10
-		end
-	end
+      assert Vector.length(row) == 10
+      assert Vector.length(col) == 10
+    end
+  end
 
-	# After I make it a supervised process
-	# describe "drop_disc/2" do
-	# 	test "expects and integer" do
-	# 		game_pid = Process.whereis(:drop_disc)
-	# 		reference = Process.monitor(game_pid)
-	# 		catch_exit do
-	# 			GameSession.drop_disc(game_pid, "1")
-	# 		end
-	# 		assert_received({:DOWN, ^reference, :process, ^game_pid, {%ArgumentError{}, _}})
-	# 	end
-	# end
+  # After I make it a supervised process
+  # describe "drop_disc/2" do
+  #   test "expects and integer" do
+  #     game_pid = Process.whereis(:drop_disc)
+  #     reference = Process.monitor(game_pid)
+  #     catch_exit do
+  #       GameSession.drop_disc(game_pid, "1")
+  #     end
+  #     assert_received({:DOWN, ^reference, :process, ^game_pid, {%ArgumentError{}, _}})
+  #   end
+  # end
 
-	describe "drop_disc/2" do
-		test "raises an error if argument is not an integer" do
-			assert_raise ArgumentError, fn ->
-				{:ok, pid } = GameSession.start_link
-				GameSession.drop_disc(pid, "1")
-			end
-		end
+  describe "drop_disc/2" do
+    test "raises an error if argument is not an integer" do
+      assert_raise ArgumentError, fn ->
+        {:ok, pid } = GameSession.start_link
+        GameSession.drop_disc(pid, "1")
+      end
+    end
 
-		#@tag :capture_log
-		test "expects and integer" do
-			{:ok, pid } = GenServer.start_link(GameSession, [])
-			board = GameSession.drop_disc(pid, 1)
+    #@tag :capture_log
+    test "expects and integer" do
+      {:ok, pid } = GenServer.start_link(GameSession, [])
+      board = GameSession.drop_disc(pid, 1)
 
-			assert Matrix.to_list(board) == [[0,0,0,0,0,0,0,0,0,0],
-														           [0,0,0,0,0,0,0,0,0,0],
-														           [0,0,0,0,0,0,0,0,0,0],
-														           [0,0,0,0,0,0,0,0,0,0],
-														           [0,0,0,0,0,0,0,0,0,0],
-														           [0,0,0,0,0,0,0,0,0,0],
-														           [0,0,0,0,0,0,0,0,0,0],
-														           [0,0,0,0,0,0,0,0,0,0],
-														           [0,0,0,0,0,0,0,0,0,0],
-														           [0,1,0,0,0,0,0,0,0,0]]
-		end
-	end
+      assert Matrix.to_list(board) == [[0,0,0,0,0,0,0,0,0,0],
+                                       [0,0,0,0,0,0,0,0,0,0],
+                                       [0,0,0,0,0,0,0,0,0,0],
+                                       [0,0,0,0,0,0,0,0,0,0],
+                                       [0,0,0,0,0,0,0,0,0,0],
+                                       [0,0,0,0,0,0,0,0,0,0],
+                                       [0,0,0,0,0,0,0,0,0,0],
+                                       [0,0,0,0,0,0,0,0,0,0],
+                                       [0,0,0,0,0,0,0,0,0,0],
+                                       [0,1,0,0,0,0,0,0,0,0]]
+    end
+  end
 
-	describe "first_empty_cell/1" do
-		test "finds the index of the first 0 (from right to left) in a vector" do
-			assert GameSession.first_empty_cell(Vector.new([0,0,0,0])) == 3
-			assert GameSession.first_empty_cell(Vector.new([0,0,0,1])) == 2
-		end
-	end
+  describe "first_empty_cell/1" do
+    test "finds the index of the first 0 (from right to left) in a vector" do
+      assert GameSession.first_empty_cell(Vector.new([0,0,0,0])) == 3
+      assert GameSession.first_empty_cell(Vector.new([0,0,0,1])) == 2
+    end
+  end
 
-	describe "update_column/1" do
-		test "replaces the first 0 (from right to left) in a vector" do
-			assert GameSession.update_column(Vector.new([0,0,0,0])) == Vector.new([0,0,0,1])
-			assert GameSession.update_column(Vector.new([0,0,0,1])) == Vector.new([0,0,1,1])
-		end
-	end
+  describe "update_column/1" do
+    test "replaces the first 0 (from right to left) in a vector" do
+      assert GameSession.update_column(Vector.new([0,0,0,0])) == Vector.new([0,0,0,1])
+      assert GameSession.update_column(Vector.new([0,0,0,1])) == Vector.new([0,0,1,1])
+    end
+  end
 
-	describe "update_board/1" do
-		test "replaces the column with the given index" do
-			board = Matrix.new([[0,0],[0,0]],2,2)
-			new_column = Vector.new([1,1])
+  describe "update_board/1" do
+    test "replaces the column with the given index" do
+      board = Matrix.new([[0,0],[0,0]],2,2)
+      new_column = Vector.new([1,1])
 
-			assert GameSession.update_board(new_column, board, 0) == Matrix.new([[1,0],[1,0]],2,2)
-		end
-	end
+      assert GameSession.update_board(new_column, board, 0) == Matrix.new([[1,0],[1,0]],2,2)
+    end
+  end
 end
